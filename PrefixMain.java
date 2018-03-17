d2a197d7f51d0ac93bccab0129d4c23e2d309534
joshuageronimo <joshuag31@hotmail.com>
2018-03-17 09:13:53 -0400

    Setup FileReader in PrefixMain & Setup Scanner in Prefix Process

--- PrefixMain.java
+++ PrefixMain.java
@@ -1,5 +1,28 @@
-public class PrefixMain {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class PrefixMain {
+
+    static private final String USER_PROMPT = "Enter the name of the prefix file you want to scan: ";
+    static private Scanner keyboard = new Scanner(System.in);
+    static private String userInput;
+
+    public static void main(String[] args) throws IOException {
+
+//        System.out.print(USER_PROMPT); /* prompt the user to enter the file name. */
+//        userInput = keyboard.nextLine(); /* get the user input. */
+        validatePrefix("prefix.txt");
+
+
+    }
+
+
+    public static void validatePrefix(String fileName) throws FileNotFoundException {
+        Scanner prefixFile = new Scanner(new FileReader(fileName)); /* Scanner object that will read the file. */
+        while (prefixFile.hasNextLine()) {
+            PrefixProcess.validateTokens(prefixFile.nextLine(),1);
+        }
     }
 }
--- PrefixProcess.java
+++ PrefixProcess.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class PrefixProcess {
 
+
+    public static Boolean validateTokens(String token, int counter) {
+
+        Scanner scan = new Scanner(token);
+
+        if (scan.hasNext()) {
+            String prefixToken = scan.next();
+            System.out.println(prefixToken);
+            if (prefixToken.equals("1")) {
+                return true;
+            } else {
+                validateTokens(scan.nextLine(),1);
+            }
+        }
+        return false;
+    }
+
+    // return true if == (+,-,*,/)
+    public static Boolean hasValidOperatorToken(String operatorToken) {
+        switch (operatorToken) {
+            case "+": case "-": case "*": case "/":
+                return true;
+            default:
+                return false;
+        }
+    }
 }
